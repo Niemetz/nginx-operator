28e0f4545cc0a5a3476f209968feea00ca1f64c4
Administrator-at-355997944876 <john.nguyen7744@gmail.com>
2024-01-14 10:32:37 -0500

    aider: Add code to list the s3 bucket called john-01-12-2024.
    # Aider chat conversation:
    
    USER: write code to list my s3 bucket called john-01-12-2024
    
    ASSISTANT: No changes are needed.

---
+++ s3-listing.py
@@ -0,0 +1,10 @@
+import boto3
+
+s3 = boto3.client('s3')
+
+def list_s3_bucket(bucket_name):
+    response = s3.list_objects_v2(Bucket=bucket_name)
+    for obj in response.get('Contents', []):
+        print(obj['Key'])
+
+list_s3_bucket('john-01-12-2024')
